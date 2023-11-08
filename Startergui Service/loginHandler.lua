0e35d64ca20d06834338eb27d924842bca0f27e6
loslolad <101575454+loslolad@users.noreply.github.com>
2023-11-08 22:09:27 +0700

    Update loginHandler.lua

--- Startergui Service/loginHandler.lua
+++ Startergui Service/loginHandler.lua
@@ -1,99 +1,120 @@
-local repl = game:GetService('ReplicatedStorage')
-local DataManger = require(script.Parent.DataManager)
+local players  				= game:GetService('Players').LocalPlayer
+local repl 				= game:GetService('ReplicatedStorage')
+local guiAnimation 			= require(repl:WaitForChild('Library'):WaitForChild('guiAnimation'))
+local ts 				= game:GetService('TweenService')
+local profileEvent 	: RemoteEvent 	= repl.Profiles
 
+
+
+local menuRegistration 	: Frame 	= script.Parent:WaitForChild('regisFrame')
+local menuLogin 	: Frame 	= script.Parent:WaitForChild('loginFrame')
+local menu 		: Frame 	= script.Parent:WaitForChild('menuFrame')
+
+
+--[Register]--
+local user		: TextBox 	= menuRegistration.Username
+local asalSekolah 	: TextBox 	= menuRegistration.AsalSekolah
+local Kelompok 		: TextBox 	= menuRegistration.Kelompok
+local createButton	: TextButton 	= menuRegistration.CreateButton.Button
+local eraseButton 	: TextButton 	= menuRegistration.EraseButton.Button
+
+--[Login]--
+local user_		: TextBox 	= menuLogin.Username
+local asalSekolah_ 	: TextBox 	= menuLogin.AsalSekolah
+local Kelompok_ 	: TextBox 	= menuLogin.Kelompok
+local loginButton_ 	: TextButton	= menuLogin.LoginButton.Button
+
+--[menu]--
+local loginButton__ : TextButton 	= menu.LoginButton.Button
+local createButton_ : TextButton 	= menu.RegisterButton.Button
+
+local currentCam = workspace.CurrentCamera
+local animPOV = ts:Create(currentCam,TweenInfo.new(.35,Enum.EasingStyle.Cubic),{FieldOfView = 125})
+local animPOV_ = ts:Create(currentCam,TweenInfo.new(.35,Enum.EasingStyle.Cubic),{FieldOfView = 90})
+local animPOV__ = ts:Create(currentCam,TweenInfo.new(.35,Enum.EasingStyle.Cubic),{FieldOfView = 70})
+currentCam.FieldOfView = 125
+
+local function visibleSelected(selected : string?)
+	for _,v in script.Parent:GetChildren() do
+		if v:isA('Frame') then
+			v.Visible = false
+		end
+	end
+	if selected == '' or selected == nil then return end
+	script.Parent:FindFirstChild(selected).Visible = true
+end
 task.spawn(function()
-	game:GetService('Players').PlayerAdded:Connect(function(pler)
-		DataManger:playerAdded(pler)
-		local getData = DataManger:GetData(pler)
-		if not getData then warn('Data null') return end
+	repl.Profiles.OnClientEvent:Connect(function(listener)
+		if listener == '' or listener == nil then warn('Listener Not Founded '..tostring(listener)) return end
 
-		local fold = Instance.new('Folder',pler)
-		fold.Name = 'leaderstats'
-
-		local benar = Instance.new('IntValue',fold)
-		benar.Name = 'Benar'
-
-
-		local salah = Instance.new('IntValue',fold)
-		salah.Name = 'Salah'
-
-
-		benar.Value = getData.Score.Benar
-		salah.Value = getData.Score.Salah
+		if listener == 'Registered' then
+			--menu.Parent.Visible = false
+			visibleSelected()
+			guiAnimation:moduleCinema1()
+			animPOV__:Play()
+			animPOV__.Completed:Wait()
+		elseif listener == 'Login' then
+			--menu.Parent.Visible = false
+			warn('Succesfully Login')
+			visibleSelected()
+			guiAnimation.FadeBloom()
+			guiAnimation.frameOut()
+			animPOV__:Play()
+			animPOV__.Completed:Wait()
+		elseif listener == 'RemoveDat' then
+			warn('Successfully Removing Data')
+		
+		end
 	end)
 end)
-repl.Profiles.OnServerEvent:Connect(function(players,Listener,Data1,Data2,Data3)
-	if not players then return end
-	if Data1 == '' and Data2 == '' and Data3 == '' then return end
-	if Listener == '' or Listener == nil then warn('Listener Not Founded '..tostring(Listener)) return end
 
-
-	local getData = DataManger:GetData(players)
-	if Listener == 'Registration' then
-		if not getData then warn('Data Failed loaded') return end
-		if getData['playerData'].userName ~= '' and getData['playerData'].asalSekolah ~= '' and getData['playerData'].kelompok ~= 0 then  
-			warn('this player already had account please login or erase your data if foget about your account') return		
-		end
-
-
-
-		getData['playerData'].userName = tostring(Data1)
-		getData['playerData'].asalSekolah = tostring(Data2)
-		getData['playerData'].kelompok = tonumber(Data3)
-		DataManger:SaveData(players)
-
-		repl.Profiles:FireClient(players,'Registered')
-
-	elseif Listener ==  'RemoveData' then
-		if not getData then warn('Data Failed loaded') return end
-		warn('Removing Data')
-
-		getData['playerData'] = {
-			['userName'] = '',
-			['kelompok'] = 0,
-			['asalSekolah'] = '',
-
-
-		}
-
-		getData['Score'] = {
-			['Benar'] = 0,
-			['Salah'] = 0
-
-		}
-
-		DataManger:SaveData(players)
-		repl.Profiles:FireClient(players,'RemoveDat')
-
-
-	elseif Listener == 'Login' then
-		if not getData then warn('Data Failed loaded') return end
-		if getData['playerData'].userName == tostring(Data1) and getData['playerData'].asalSekolah == tostring(Data2) and getData['playerData'].kelompok == tonumber(Data3) then
-			repl.Profiles:FireClient(players,'Login')
-			print('Server Login Success')
-		else warn('Incorrect, try again') return
+task.spawn(function()
+	for _,v in script.Parent:GetDescendants() do 
+		if v:isA('ImageButton') and v.Name == 'Back' then
+			v.MouseButton1Click:Connect(function()
+				v.Parent.Visible = false
+				script.Parent.menuFrame.Visible = true
+				animPOV:Play()
+				animPOV.Completed:Wait()
+			end)
 		end
 	end
 end)
 
 
-repl:WaitForChild('Question_').OnServerEvent:Connect(function(players,listener,data2,data3)
-	if data2 == nil or data2 == '' then warn('Data2 Not Founded') return end
-	if data3 == nil or data3 == '' then warn('Data3 Not Founded') return end
-	local getData = DataManger:GetData(players)
-	local lead : Folder= players:FindFirstChild('leaderstats')
-
-	if not lead	then return end
-	if not getData then warn('Data Failed loaded') return end
-	if listener == 'Answer' then
-		if data3 == data2['Jawaban'] then getData.Score.Benar += 1	 print('Jawaban Benar')			
-		else getData.Score.Salah += 1 print('Jawaban Salah') end 
-	elseif listener == 'kosong' then getData.Score.Salah += 3 warn('Jawaban Kosong')	
-	else warn('listener Not Founded') return end
-	lead:WaitForChild('Benar').Value = getData.Score.Benar
-	lead:WaitForChild('Salah').Value = getData.Score.Salah
-
-	DataManger:SaveData(players)
-
-
+createButton.MouseButton1Click:Connect(function()
+	if user.Text == '' and asalSekolah.Text == '' and Kelompok.Text == '' or tonumber(Kelompok.Text) == nil or tonumber(Kelompok.Text) ==  0 then 
+		warn('Password or Userame or kelompok are null')
+		return	
+	end
+	profileEvent:FireServer('Registration',user.Text,asalSekolah.Text,tonumber(Kelompok.Text))
 end)
+
+eraseButton.MouseButton1Click:Connect(function()
+	profileEvent:FireServer('RemoveData')
+	warn('Data Erased')
+end)
+
+
+loginButton_.MouseButton1Click:Connect(function()
+	if user_.Text == '' and asalSekolah_.Text == '' and  Kelompok_.Text == '' or tonumber(Kelompok_.Text) == nil or tonumber(Kelompok_.Text) ==  0  then 	
+		warn('Password or Userame are null')
+		return 
+	end
+	profileEvent:FireServer('Login',user_.Text,asalSekolah_.Text,tonumber(Kelompok_.Text))
+	
+end)
+
+loginButton__.MouseButton1Click:Connect(function()
+	visibleSelected('loginFrame')
+	animPOV_:Play()
+	animPOV_.Completed:Wait()
+end)
+
+createButton_.MouseButton1Click:Connect(function()
+	visibleSelected('regisFrame')
+	animPOV_:Play()
+	animPOV_.Completed:Wait()
+end)
+
+
